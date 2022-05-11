fd8afc2599538f8f24c6051ef8c4e1bf9db5d135
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2022-05-11 13:20:47 +0800

    Optimize DB schema

--- database-schema/mysql.sql
+++ database-schema/mysql.sql
@@ -10,16 +10,6 @@ CREATE TABLE IF NOT EXISTS `session`(
     `not_after` TEXT NOT NULL,
     FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
 );
-CREATE TABLE IF NOT EXISTS `bookmark`(
-    `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `user_id` INTEGER NOT NULL,
-    `order_id` REAL NOT NULL,
-    `name` TEXT NOT NULL,
-    `url` TEXT NOT NULL,
-    `add_time` TEXT NOT NULL,
-    `favicon` TEXT,
-    FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
-);
 CREATE TABLE IF NOT EXISTS `collection`(
     `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `user_id` INTEGER NOT NULL,
@@ -27,14 +17,15 @@ CREATE TABLE IF NOT EXISTS `collection`(
     `name` TEXT NOT NULL,
     FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
 );
-CREATE TABLE IF NOT EXISTS `bookmark_collection`(
+CREATE TABLE IF NOT EXISTS `bookmark`(
     `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `user_id` INTEGER NOT NULL,
-    `bookmark_id` INTEGER NOT NULL UNIQUE,
-    `collection_id` INTEGER NOT NULL,
+    `order_id` REAL NOT NULL,
+    `name` TEXT NOT NULL,
+    `url` TEXT NOT NULL,
+    `add_time` TEXT NOT NULL,
+    `collection_id` INTEGER,
+    `favicon` TEXT,
     FOREIGN KEY(`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY(`collection_id`) REFERENCES `collection`(`id`) ON DELETE RESTRICT ON UPDATE NO ACTION,
-    FOREIGN KEY(`bookmark_id`) REFERENCES `bookmark`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    FOREIGN KEY(`collection_id`) REFERENCES `collection`(`id`) ON DELETE SET NULL
 );
--- CREATE UNIQUE INDEX IF NOT EXISTS `bookmark_collection_bookmark_id_collection_id` ON
---     `bookmark_collection`(`bookmark_id`);
--- database-schema/sqlite.sql
+++ database-schema/sqlite.sql
@@ -6,22 +6,10 @@ CREATE TABLE IF NOT EXISTS "bookmark" (
     "url" text NOT NULL,
     "add_time" text NOT NULL,
     "favicon" text,
-    FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE
-  );
-  
-  
-  CREATE TABLE IF NOT EXISTS "bookmark_collection" (
-    "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "user_id" integer NOT NULL,
-    "bookmark_id" integer NOT NULL UNIQUE,
-    "collection_id" integer NOT NULL,
+    "collection_id" integer,
     FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE,
-    FOREIGN KEY ("collection_id") REFERENCES "collection" ("id") ON DELETE RESTRICT ON UPDATE NO ACTION,
-    FOREIGN KEY ("bookmark_id") REFERENCES "bookmark" ("id") ON DELETE CASCADE ON UPDATE NO ACTION
+    FOREIGN KEY ("collection_id") REFERENCES "collection" ("id") ON DELETE SET NULL
   );
-  
-  /*CREATE UNIQUE INDEX IF NOT EXISTS "bookmark_collection_bookmark_id_collection_id" ON "bookmark_collection" ("bookmark_id");*/
-  
   
   CREATE TABLE IF NOT EXISTS "collection" (
     "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
